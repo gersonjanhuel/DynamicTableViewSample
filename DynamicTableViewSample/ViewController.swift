682a7357abee7a8405cb860a26e2007325a947b6
Gerson Janhuel <gersonjanhuelhuel@gmail.com>
2020-08-11 14:43:33 +0700

    Initial Commit

---
+++ DynamicTableViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTableViewSample
+//
+//  Created by Gerson Janhuel on 11/08/20.
+//  Copyright © 2020 Elcode Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
